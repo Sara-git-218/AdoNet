bf85e9a9f5a0d42c7c48302edeca394f37ba78d5
Sara-git-218 <sarah74218@gmail.com>
2025-04-23 11:52:08 +0300

    finish...

---
+++ AdoNet/DataAcces.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data;
+namespace AdoNet
+{
+    class DataAcces
+    {
+
+        internal int InsertDataProd(string connectionString)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter product name");
+            string product_name = Console.ReadLine();
+            Console.WriteLine("enter catgory");
+            string cagory = Console.ReadLine();
+            Console.WriteLine("enter price");
+            string price = Console.ReadLine();
+            Console.WriteLine("enter product description");
+            string product_Description = Console.ReadLine();
+            Console.WriteLine("enter product imageurl");
+            string imageurl = Console.ReadLine();
+
+            string query = "INSERT INTO Products(ProductName,CatgoryID,Price,ProductDesdription,ImageUrl)" +
+                "VALUES(@product_name,@catgory,@price,@product_Description,@imageurl)";
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                cmd.Parameters.Add("@product_name", SqlDbType.NVarChar, 50).Value = product_name;
+                cmd.Parameters.Add("@catgory", SqlDbType.SmallInt).Value = cagory;
+                cmd.Parameters.Add("@price", SqlDbType.SmallInt).Value = price;
+                cmd.Parameters.Add("@product_Description", SqlDbType.NVarChar, 50).Value = product_Description;
+                cmd.Parameters.Add("@imageurl", SqlDbType.NVarChar, 50).Value = imageurl;
+                cn.Open();
+                int rowsAffect = cmd.ExecuteNonQuery();
+                cn.Close();
+                return rowsAffect;
+            }
+
+        }
+        internal int InsertDataCatgory(string connectionString)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter CatgoryName");
+            string catgory_name = Console.ReadLine();
+           
+
+            string query = "INSERT INTO Catgories(CatgoryName)" +
+                "VALUES(@catgory_name)";
+            using (SqlConnection cn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(query, cn))
+            {
+                cmd.Parameters.Add("@catgory_name", SqlDbType.NVarChar, 50).Value =catgory_name;
+                
+                cn.Open();
+                int rowsAffect = cmd.ExecuteNonQuery();
+                cn.Close();
+                return rowsAffect;
+            }
+
+        }
+        internal void readDataProd(string connectionString)
+        {
+            string query = "select * from Products";
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}\t{2}\t{3}\t{4}\t{5}",
+                            reader[0], reader[1], reader[2], reader[3], reader[4], reader[5]);
+
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                //Console.ReadLine();
+            }
+        }
+        internal void readDataCatgory(string connectionString)
+        {
+            string query = "select * from Catgories";
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("\t{0}\t{1}",
+                            reader[0], reader[1]);
+
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                //Console.ReadLine();
+            }
+        }
+    }
+}
--- AdoNet/Program.cs
+++ AdoNet/Program.cs
@@ -1,68 +1,22 @@
 ﻿using System.Data.SqlTypes;
 using Microsoft.Data.SqlClient;
 using System.Data;
-internal class Program
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace AdoNet
 {
-    public int InsertData(string connectionString)
-    {
- Console.WriteLine("Hello, World!");
-        Console.WriteLine("enter product name");
-        string product_name = Console.ReadLine();
-        Console.WriteLine("enter catgory");
-        string cagory = Console.ReadLine();
-        Console.WriteLine("enter price");
-        string price = Console.ReadLine();
-        Console.WriteLine("enter product description");
-        string product_Description = Console.ReadLine();
-        Console.WriteLine("enter product imageurl");
-        string imageurl = Console.ReadLine();
+    internal class Program
 
-        string query = "INSERT INTO Products(ProductName,CatgoryID,Price,ProductDescription)" +
-            "VALUES(@product_name,@cagory,@price,@product_Description,@imageurl)";
-        using (SqlConnection cn = new SqlConnection(connectionString)) 
-        using (SqlCommand cmd = new SqlCommand(query, cn))
+    {
+
+        private static void Main(string[] args)
         {
-            cmd.Parameters.Add("@product_name", SqlDbType.NVarChar, 50).Value = product_name;
-            cmd.Parameters.Add("@catgory", SqlDbType.SmallInt).Value = cagory;
-            cmd.Parameters.Add("@price", SqlDbType.SmallInt).Value = price;
-            cmd.Parameters.Add("@product_Description", SqlDbType.NVarChar, 50).Value = product_Description;
+            string connectionString = " Data Source = SRV2\\PUPILS; Initial Catalog = 326059268_ShopApi; Integrated Security = True; Trust Server Certificate = True";
 
-            cn.Open();
-            int rowsAffect = cmd.ExecuteNonQuery();
-            cn.Close();
-            return rowsAffect;
+            DataAcces dataAcces = new DataAcces();
+            //int v =dataAcces.InsertDataProd(connectionString);
+            //dataAcces.readDataProd(connectionString);
+            int a = dataAcces.InsertDataCatgory(connectionString);
+            dataAcces.readDataCatgory(connectionString);
         }
-
-    }
-    internal void readData(string connectionString)
-    {
-        string query= "select * from Products";
-        using(SqlConnection connection=
-            new SqlConnection(connectionString))
-        {
-            SqlCommand cmd = new SqlCommand(query, connection);
-            try
-            {
-                connection.Open();
-                SqlDataReader reader = cmd.ExecuteReader();
-                while(reader.Read())
-                {
-                    Console.WriteLine(
-                        "\t{1}\t{2}\t{3}\t{4}\t{5}",
-                        reader[0], reader[1], reader[2], reader[3], reader[5]);
-                }
-                reader.Close();
-            }
-            catch(Exception ex)
-                {
-                Console.WriteLine(ex.Message);
-            }
-            Console.ReadLine();
-        }
-    }
-    private static void Main(string[] args)
-    {
-        string connectionString=""
-       
     }
 }
